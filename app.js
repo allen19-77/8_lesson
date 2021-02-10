1515c4a0aa58bf30df8dad904e4b296d49144be6
allen19-77 <63224818+allen19-77@users.noreply.github.com>
2021-02-10 19:43:41 +0500

    second

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
-let myInput = document.createElement('input')
-myInput.setAttribute('type', 'number')
-let myButton = document.createElement('button')
-myButton.innerText = 'Показать'
-let myParagraf = document.createElement('p')
-myParagraf.innerText = ''
-
 let body = document.querySelector('body')
-body.appendChild(myInput)
-body.appendChild(myButton)
-body.appendChild(myParagraf)
 
-myButton.addEventListener('click', function () {
+//for (начало = 0; 5 раз; счетчик = 5 )
+for (let i = 6; i <= 10; i++) {
+	let header = document.createElement('h1')
+	header.innerText = `${i} человек`
+	body.appendChild(header)
+}
 
-	let price = 100
-	let age = myInput.value
-
-	//если младше 8 лет, цена 0
-	if (age < 8) {
-		price = 0 //0
-	} else if (age < 20) {
-		price = price * 0.8 //100 * 0.8 = 80
-	} else if (age > 20) {
-		price = price //100
-	}
-
-	myParagraf.innerText = `Цена билета равна ${price}`
-})
